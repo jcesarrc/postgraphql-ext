19c1ada8522c51242027e5afa0dbb5f60af6cf6d
jcesarrc <jcesarrc@gmail.com>
2017-06-05 11:03:35 -0500

    OK

--- src/auth/jwtredis.js
+++ src/auth/jwtredis.js
@@ -1,12 +1,26 @@
-var app         = express();
 var express     = require('express');
+var app         = express();
+var redis       = require('redis');
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 
-var jwt    = require('jsonwebtoken');
 var config = require('./config');
 var User   = require('./models/user');
+
+var JWTRedisSession = require("jwt-redis-session");
+var redisClient = redis.createClient({host:"138.197.51.117", port:"6379"});
+var secret = config.secret;
+
+app.use(JWTRedisSession({
+	client: redisClient,
+	secret: secret,
+	keyspace: "sess:",
+	maxAge: 3600,
+	algorithm: "HS256",
+	requestKey: "jwtSession",
+	requestArg: "accessToken"
+}));
 
 // configuration =========
 var port = process.env.PORT || 8080;
@@ -20,12 +34,10 @@ app.use(bodyParser.json());
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
-// routes ================
-
 var apiRoutes = express.Router();
 
 apiRoutes.post('/authenticate', function(req, res) {
-  // find the user
+
   User.findOne({
     name: req.body.name
   }, function(err, user) {
@@ -35,17 +47,23 @@ apiRoutes.post('/authenticate', function(req, res) {
     if (!user) {
       res.json({ success: false, message: 'Authentication failed. User not found.' });
     } else if (user) {
-
       if (user.password != req.body.password) {
         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
       } else {
-        var token = jwt.sign(user, app.get('superSecret'), { });
-
-        res.json({
-          success: true,
-          message: 'Token delivered!',
-          token: token
-        });
+        //var token = jwt.sign(user, app.get('superSecret'), { });
+        req.jwtSession.user = user.toJSON();
+    		// this will be attached to the JWT
+    		var claims = {
+    			iss: "Grapheno",
+    			aud: "grapheno.io"
+    		};
+    		req.jwtSession.create(claims, function(error, token){
+          res.json({
+            success: true,
+            message: 'Token delivered!',
+            token: token
+          });
+    		});
       }
     }
   });
@@ -57,7 +75,18 @@ apiRoutes.use(function(req, res, next) {
 
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
+  console.log("Request JWT session data: ",
+		req.jwtSession.id,
+		req.jwtSession.claims,
+		req.jwtSession.jwt
+	);
 
+  console.log(req.jwtSession.toJSON());
+
+
+  res.json(req.jwtSession.toJSON());
+
+ /*
   if (token) {
     jwt.verify(token, app.get('superSecret'), function(err, decoded) {
       if (err) {
@@ -70,14 +99,12 @@ apiRoutes.use(function(req, res, next) {
     });
 
   } else {
-
-
     return res.status(403).send({
         success: false,
         message: 'No token provided.'
     });
-
   }
+  */
 });
 
 apiRoutes.get('/users', function(req, res) {
